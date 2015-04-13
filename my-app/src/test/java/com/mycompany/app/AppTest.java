84000d933676710bc6b376601035c9a335ebc7e2
Goatic <Goatic@live.dk>
2015-04-13 09:05:56 +0200

    HangmanTest.java

--- my-app/src/test/java/com/mycompany/app/AppTest.java
+++ my-app/src/test/java/com/mycompany/app/AppTest.java
@@ -1,38 +1,68 @@
-package com.mycompany.app;
+import static org.junit.Assert.*;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+public class HangmanTest {
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	static GameProvider game;
+	static int guesses;
+	static String[] words;
+	
+	@Before
+	public void setUp() throws Exception {
+		words = new String[] {"xyz", "zyx"};
+		guesses = 7;
+		game = new GameProvider(words, guesses);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		words = null;
+		guesses = -1;
+		game = null;
+	}
+
+	@Test
+	public void instantiationTest() {
+		Assert.assertEquals(guesses, game.guesses);
+		
+		Assert.assertNotNull(game.word);
+		Assert.assertNotNull(game.guessed);
+		Assert.assertNotNull(game.guesses);
+	}
+	
+	@Test
+	public void guessTest() {
+		game.guess('y');
+		Assert.assertEquals(guesses-1, game.guesses);
+		Assert.assertFalse(game.done());
+		
+	}
+	
+	@Test
+	public void winTest() {
+		for(int x=0; x<guesses-2; x++) {
+			game.guess('x');
+		}
+		game.guess('y');
+		game.guess('z');
+		Assert.assertTrue(game.won());
+		Assert.assertFalse(game.lost());
+		Assert.assertTrue(game.done());
+	}
+	
+	@Test
+	public void loseTest() {
+		for(int x=0; x<guesses; x++) {
+			game.guess('y');
+		}
+		Assert.assertTrue(game.lost());
+		Assert.assertFalse(game.won());
+		Assert.assertTrue(game.done());
+	}
+
 }
